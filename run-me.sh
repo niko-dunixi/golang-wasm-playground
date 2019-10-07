567f7f9abce5689ef84596e749948fd1f29dd78b
Paul Baker <paul.nelson.baker@gmail.com>
2019-10-07 13:14:01 -0600

    The run-me script now will remove files I ask it to remove before building.

--- run-me.sh
+++ run-me.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set +e
 set -x
+
+for file_to_rm in "${@}"; do
+  if [ -f "${file_to_rm}" ]; then
+    rm -v "${file_to_rm}"
+  fi
+done
+
 if [ -f assets_vfsdata.go ]; then
   rm assets_vfsdata.go
 fi

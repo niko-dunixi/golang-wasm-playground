cfc4447fb4e5b4095367091018cc5c62d3e5b40b
Paul Baker <paul.nelson.baker@gmail.com>
2019-10-07 13:14:46 -0600

    checkerboard seems to work, but I don't understand why each iteration resets the fill and stroke style.

--- cellular-automata/checkerboard/main.go
+++ cellular-automata/checkerboard/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"syscall/js"
 )
 
@@ -8,34 +9,57 @@ func main() {
 	runForever := make(chan bool)
 	window := js.Global()
 	canvas := window.Get("document").Call("getElementById", "canvas")
+	window.Call("addEventListener", "resize", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		width, height := updateCanvasSize(window, canvas)
+		fmt.Printf("resize triggered: %f x %f\n", width, height)
+		return nil
+	}))
+	_, _ = updateCanvasSize(window, canvas)
+
 	canvasCtx := canvas.Call("getContext", "2d")
-	canvasCtx.Set("fillStyle", "red")
-	canvasCtx.Set("strokeStyle", "red")
-	canvasCtx.Set("lineWidth", 5)
 
 	var renderer js.Func
 	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		// Make sure the canvas stretches the whole way through the page
-		height := window.Get("innerHeight").Float()
-		width := window.Get("innerWidth").Float()
-		canvas.Set("innerHeight", height)
-		canvas.Set("innerWidth", width)
+		width, height := getWindowSize(window)
 		canvasCtx.Call("clearRect", 0, 0, width, height)
+		canvasCtx.Set("fillStyle", "red")
+		canvasCtx.Set("strokeStyle", "red")
+		canvasCtx.Set("lineWidth", 5)
+		fmt.Printf("Renderer called: %s %s %f",
+			canvasCtx.Get("fillStyle").String(), canvasCtx.Get("strokeStyle").String(), canvasCtx.Get("lineWidth").Float())
 		{
 			squareWidth := width / 8
 			squareHeight := height / 8
+			offsetWidth := squareWidth * 0.15
+			offsetHeight := squareHeight * 0.15
 			for i := 0; i < 8; i++ {
 				for j := 0; j < 8; j++ {
-					//canvasCtx.Call("fillRect", float64(i)*squareWidth, float64(j)*squareHeight, squareWidth, squareHeight)
-					canvasCtx.Call("strokeRect", float64(i)*squareWidth, float64(j)*squareHeight, squareWidth, squareHeight)
+					if (i+j)%2 == 0 {
+						canvasCtx.Call("strokeRect", float64(i)*squareWidth+offsetWidth, float64(j)*squareHeight+offsetHeight,
+							squareWidth-(2*offsetWidth), squareHeight-(2*offsetHeight))
+					} else {
+						canvasCtx.Call("fillRect", float64(i)*squareWidth, float64(j)*squareHeight, squareWidth, squareHeight)
+					}
 				}
 			}
 		}
-		// request a frame draw
 		window.Call("requestAnimationFrame", renderer)
 		return nil
 	})
 	window.Call("requestAnimationFrame", renderer)
 
 	<-runForever
+}
+
+func getWindowSize(window js.Value) (float64, float64) {
+	width := window.Get("innerWidth").Float()
+	height := window.Get("innerHeight").Float()
+	return width, height
+}
+
+func updateCanvasSize(window js.Value, canvas js.Value) (float64, float64) {
+	width, height := getWindowSize(window)
+	canvas.Set("width", width)
+	canvas.Set("height", height)
+	return width, height
 }

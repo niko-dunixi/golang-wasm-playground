3790ffb1562bca54dc8c2917defcd081e34ec15e
Paul Baker <paul.nelson.baker@gmail.com>
2019-10-07 15:08:52 -0600

    Fixing this thing because you seem to need to reset the styles after clearRect

--- cellular-automata/checkerboard/main.go
+++ cellular-automata/checkerboard/main.go
@@ -22,27 +22,32 @@ func main() {
 	renderer = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		width, height := getWindowInnerSize(window)
 		canvasCtx.Call("clearRect", 0, 0, width, height)
+		fmt.Printf("Rendering with: Canvas Size %fx%f  fillStyle=%s strokeStyle%s lineWidth=%f\n",
+			width, height,
+			canvasCtx.Get("fillStyle").String(),
+			canvasCtx.Get("strokeStyle").String(),
+			canvasCtx.Get("lineWidth").Float(),
+		)
+		canvasCtx.Call("beginPath")
 		canvasCtx.Set("fillStyle", "red")
 		canvasCtx.Set("strokeStyle", "red")
 		canvasCtx.Set("lineWidth", 5)
-		fmt.Printf("Renderer called: %s %s %f\n",
-			canvasCtx.Get("fillStyle").String(), canvasCtx.Get("strokeStyle").String(), canvasCtx.Get("lineWidth").Float())
-		{
-			squareWidth := width / 8
-			squareHeight := height / 8
-			offsetWidth := squareWidth * 0.15
-			offsetHeight := squareHeight * 0.15
-			for i := 0; i < 8; i++ {
-				for j := 0; j < 8; j++ {
-					if (i+j)%2 == 0 {
-						canvasCtx.Call("strokeRect", float64(i)*squareWidth+offsetWidth, float64(j)*squareHeight+offsetHeight,
-							squareWidth-(2*offsetWidth), squareHeight-(2*offsetHeight))
-					} else {
-						canvasCtx.Call("fillRect", float64(i)*squareWidth, float64(j)*squareHeight, squareWidth, squareHeight)
-					}
+
+		squareWidth := width / 8
+		squareHeight := height / 8
+		offsetWidth := squareWidth * 0.15
+		offsetHeight := squareHeight * 0.15
+		for i := 0; i < 8; i++ {
+			for j := 0; j < 8; j++ {
+				if (i+j)%2 == 0 {
+					canvasCtx.Call("strokeRect", float64(i)*squareWidth+offsetWidth, float64(j)*squareHeight+offsetHeight,
+						squareWidth-(2*offsetWidth), squareHeight-(2*offsetHeight))
+				} else {
+					canvasCtx.Call("fillRect", float64(i)*squareWidth, float64(j)*squareHeight, squareWidth, squareHeight)
 				}
 			}
 		}
+		canvasCtx.Call("closePath")
 		window.Call("requestAnimationFrame", renderer)
 		return nil
 	})
